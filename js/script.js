066c7d1978ea1a4b71e953ae04d6ac483725396a
Khalid Yossry <67026475+khalidyossry@users.noreply.github.com>
2022-11-20 17:13:27 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function one(){
+    document.getElementById("butone").style.display="none";
+    document.getElementById("buttwo").style.display="block";
+}
+function two(){
+    document.getElementById("buttwo").style.display="none";
+    document.getElementById("butthree").style.display="block";
+}
+function three(){
+    document.getElementById("butthree").style.display="none";
+    document.getElementById("cake").style.display="block";
+    document.getElementById("bdc").style.display="block";
+}
